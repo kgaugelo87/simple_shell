e6e3f7589030abd3791fbc9f30715e834fe1bfb2
kgaugelo87 <kgaugelo.maile@gmail.com>
2023-09-14 11:16:00 -0700

    shell main.h file

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+
+int main(int ac, char **av);
+int main(void);
+int main(int ac, char **av, char **env);
+
+/** 
+ * Getenv
+ */
+char *_getenv(const char *name);
+
+/**
+ * setenv
+ */
+int _setenv(const char *name, const char *value, int overwrite);
+
+/**
+ * unsetenv
+ */
+int _unsetenv(const char *name);
+
+#endif
