18da9db477ffde33cdfc8ba21900c5e9359ab512
kgaugelo87 <kgaugelo.maile@gmail.com>
2023-09-21 06:54:19 -0700

    updated main header file

--- main.h
+++ main.h
@@ -4,12 +4,11 @@
 #include <stdio.h>
 #include <cstring>
 #include <string.h>
+#include <unistd.h>
 
 int main(int ac, char **av);
 int main(void);
 int main(int ac, char **av, char **env);
-int _change_dir(info_t *info);
-int _curenviron(info_t *details);
 
 /** 
  * Getenv
